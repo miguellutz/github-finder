2be6ac9c9ee7368201d6a13f3b598289f601682b
Miguel Lutz <miguellutz31@gmail.com>
2022-06-21 14:12:42 +0200

    JSX vs plain JS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 class App extends Component {
   render() {
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello from React')
+    // );
     return (
       <div className="App">
         <h1>Hello from React</h1>
